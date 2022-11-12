79175de9a7e3d4155bb1e22e099d35f10e6c8b07
DELL <DELL@DESKTOP-HR0BL1L>
2022-11-12 20:30:17 +0530

    Section11: OOPS in Detail

---
+++ Selenium_WebDriver_with_Java/src/section11_LiveClasses/Encapsulation.java
@@ -0,0 +1,10 @@
+package section11_LiveClasses;
+
+public class Encapsulation {
+	
+	/*
+	 * Wrapping of data(variable) and functions in a single unit is called encapsulation.
+	 * In Encapsulation the variable of a class will be hidden from other class , and can be
+	 * accessed throughout the methods of their current class. Therefore it is also known as data Hiding.
+	 */
+}
