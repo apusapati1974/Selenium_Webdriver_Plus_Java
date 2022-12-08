11943971f5900cfd4cba2d5c4ff22691b4c15e67
DELL <DELL@DESKTOP-HR0BL1L>
2022-12-08 12:36:17 +0530

    Section - 12 : Final Keyword

---
+++ Selenium_WebDriver_with_Java/src/section12_LiveClasses/Final_Keyword_Example1.java
@@ -0,0 +1,11 @@
+package section12_LiveClasses;
+/* If we want to make the variable as constant we can use final keyword.
+ * If u r using the final keyword in class means it's a final class which cannot be inherited*/
+public class Final_Keyword_Example1 {
+	
+	int i=10;
+	
+	public final void sumMethod(int a,int b) {
+		System.out.println("The sum of a & b is "+a+b);
+	}
+}
---
+++ Selenium_WebDriver_with_Java/src/section12_LiveClasses/Final_Keyword_Example2.java
@@ -0,0 +1,11 @@
+package section12_LiveClasses;
+
+public class Final_Keyword_Example2 extends Final_Keyword_Example1{
+	
+	/*
+	 * @Override  //  but when the method is final then overriding concept will not word and throw and error.
+	public void sumMethod(int a,int b) {
+		System.out.println("The sum of a & b is "+a+b);
+	}
+	 */
+}
